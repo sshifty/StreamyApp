7d471019c9511e75c6d4b00549745342b670a93b
sshifty <atiszon@gmail.com>
2019-05-03 14:43:02 +0200

    first commit

---
+++ client/src/components/GoogleAuth.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn, signOut } from "../actions";
+class GoogleAuth extends Component {
+  
+  componentDidMount() {
+    window.gapi.load("client:auth2", () => {
+      window.gapi.client
+        .init({
+          clientId:
+            "994815556148-d61c7qju15lvd31qjm8o6mj04olclkul.apps.googleusercontent.com",
+          scope: "email"
+        })
+        .then(() => {
+          this.auth = window.gapi.auth2.getAuthInstance();
+          this.onAuthChange(this.auth.isSignedIn.get())
+          this.auth.isSignedIn.listen(this.onAuthChange);
+        });
+    });
+  }
+
+  onAuthChange = isSignedIn => {
+    if(isSignedIn){
+        this.props.signIn(this.auth.currentUser.get().getId());//action creatornek átadjuk az id-t argumentmként as a payload property
+    }else{
+        this.props.signOut();
+    }
+  };
+
+  onSignOutClick = () => {
+    this.auth.signOut();
+  };
+  onSignInClick = () => {
+    this.auth.signIn();
+  };
+  renderAuthButton() {
+    if (this.props.isSignedIn === null) {
+      return null;
+    } else if (this.props.isSignedIn) {
+      return (
+        <button className="ui red google button" onClick={this.onSignOutClick}>
+          <i className="google icon" />
+          Sign out!
+        </button>
+      );
+    } else {
+      return (
+        <button className="ui red google button" onClick={this.onSignInClick}>
+          <i className="google icon" />
+          Sign In With Google!
+        </button>
+      );
+    }
+  }
+  render() {
+    return <div>{this.renderAuthButton()}</div>;
+  }
+}
+
+const mapStateToProps=(state)=>{    
+    return {
+        isSignedIn:state.auth.isSignedIn
+    }
+};
+
+export default connect(
+  mapStateToProps,
+  { signIn, signOut }
+)(GoogleAuth);
---
+++ client/src/reducers/streamReducer.js
@@ -0,0 +1,27 @@
+import {
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    DELETE_STREAM,
+    EDIT_STREAM
+}from '../actions/types';
+
+import _ from 'lodash';
+
+export default(state={},action)=>{
+    switch(action.type){
+        case FETCH_STREAMS:
+            return {...state,..._.mapKeys(action.payload,'id')};
+        case FETCH_STREAM:
+            return {...state,[action.payload.id]:action.payload};
+        case CREATE_STREAM:
+            return {...state,[action.payload.id]:action.payload};
+        case EDIT_STREAM:
+            return {...state,[action.payload.id]:action.payload};
+        case DELETE_STREAM:
+            return _.omit(state,action.payload);// deleteStream actionnel a payload csak az id-t tartalmazza 
+        default:
+            return state;
+    }
+}
+
